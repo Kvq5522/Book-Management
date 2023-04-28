25b464ac5cd3c045744360d8c477fd044591bd33
HiusHa1 <hahuynhduchuy@gmail.com>
2023-04-28 18:41:27 +0700

    add add new author and edit author form

--- public/scripts/dashboardAuthor.js
+++ public/scripts/dashboardAuthor.js
@@ -10,3 +10,29 @@ toggleButton.onclick = function () {
 
 //others
 let navbar_highlight = document.getElementById('author-link').classList.add('active');
+
+let add_btn = document.getElementById('add-author-btn');
+let close_add_section = document.getElementById('close-Au-btn');
+let edit_btn = document.getElementById("edit-author-btn");
+let close_edit_section = document.getElementById("close-edit-btn");
+
+
+add_btn.addEventListener('click', () => {
+    let add_section = document.getElementById('add-au');
+    add_section.style.display = 'block';
+})
+
+close_add_section.addEventListener('click', () =>{
+    let add_section = document.getElementById("add-au");
+    add_section.style.display = "none";
+})
+
+edit_btn.addEventListener('click', ()=> {
+    let edit_section = document.getElementById("edit-author");
+    edit_section.style.display ='block';
+})
+
+close_edit_section.addEventListener('click', () => {
+    let edit_section = document.getElementById("edit-author");
+    edit_section.style.display = "none";
+})
